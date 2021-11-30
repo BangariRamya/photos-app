20ed3fabb58da12fd6ecacfc0fcbbd5bea868e7c
ramya <bangariramya444@gmail.com>
2021-11-30 12:59:09 +0530

    photos-app

--- src/photosComponent.jsx
+++ src/photosComponent.jsx
@@ -10,29 +10,65 @@ import {
 } from "reactstrap";
 
 const Photos = () => {
-  const [state, setState] = useState([]);
+  const [state, setState] = useState([]);  //This is for data we want to change and update, while fetching(refer below useEffect)
   const [spin, setSpin] = useState(false);
   const [page, setPage] = useState(1);
+  
 
   const pageUpdate = (num) => {
     setPage(num);
   };
+  
+  useEffect(() => {
+    try{
+      setSpin(true);
+      fetch(`https://jsonplaceholder.typicode.com/albums/${page}/photos`)
+        .then((resp) => resp.json())
+        .then((data) => {
+          // console.log(data)
+          setState(data);
+          setSpin(false);
+          // setPage(null);
+          //console.log(page);
+        });
+    }  //try must followed by either catch or finally ...if not gives error
+    catch (error)
+    {
+      setSpin(false)
+      console.log(error);
+    };
+  }, [page]);
+  
+  /*...Fetching using async and await and then calling this function in useEffect..;;getting output but getting warning as missing dependency...see below at useEffect ending
+  const fetchData =  async () => {
+      try{
+       setSpin(true);
+       const resp = await fetch(`https://jsonplaceholder.typicode.com/albums/${page}/photos`);
+       const data = await resp.json();
+       
+       //console.log(data);
+       setState(data);
+       setSpin(false);
+      }
+      catch(error)
+      {
+        setSpin(false);
+        console.log(error);
+      }
+  }
 
   useEffect(() => {
-    setSpin(true);
-    fetch(`https://jsonplaceholder.typicode.com/albums/${page}/photos`)
-      .then((resp) => resp.json())
-      .then((data) => {
-        // console.log(data)
-        setState(data);
-        setSpin(false);
-        // setPage(null);
-        //console.log(page);
-      });
-  }, [page]);
+  
+           fetchData();
+
+        }, [page]);  //gives warning as : React Hook useEffect has a missing dependency: 'fetchData'. Either include it or remove the dependency array  react-hooks/exhaustive-deps
+  */
 
   return (
     <div>
+
+      <h1>Photos App</h1>
+
       {/* <Spinner style={{alignItem: "left"}}>Loading...</Spinner>       */}
       {spin && <Spinner>Loading...</Spinner>}
       <Container>
@@ -78,7 +114,7 @@ const Photos = () => {
           <PaginationItem>
             <PaginationLink href="#" last />
           </PaginationItem>
-        </Pagination>
+        </Pagination> 
       </div>
     </div>
   );
